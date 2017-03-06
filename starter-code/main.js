01da52d8032c20a58d5351f7ff8d9f8be603fb1d
Louis Boyle <louis@Louiss-MacBook-Pro.local>
2017-03-06 16:44:25 +0000

    unit 9 assignment

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,5 +1,11 @@
 console.log("JS file is connected to HTML! Woo!")
-var cardOne = "King";
-var cardTwo = "King";
-var cardThree = "Queen";
-var cardFour = "Queen";
+var cardOne = "king";
+var cardTwo = "king";
+var cardThree = "queen";
+var cardFour = "queen";
+
+if (cardTwo === cardFour) {
+	alert("You found a match!");
+} else {
+	alert("Sorry, try again.");
+}
