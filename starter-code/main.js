51d1117cf3d56d7992bc3b74f7e4ea6eae4b4875
Louis Boyle <louis@Louiss-MacBook-Pro.local>
2017-03-07 18:06:55 +0000

    unit 10 assignment

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,11 +1,20 @@
 console.log("JS file is connected to HTML! Woo!")
-var cardOne = "king";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "queen";
+// var cardOne = "king";
+// var cardTwo = "king";
+// var cardThree = "queen";
+// var cardFour = "queen";
 
-if (cardTwo === cardFour) {
-	alert("You found a match!");
-} else {
-	alert("Sorry, try again.");
-}
+// if (cardTwo === cardFour) {
+// 	alert("You found a match!");
+// } else {
+// 	alert("Sorry, try again.");
+// }
+var createCards = function() {
+	var gameBoard = document.getElementById('game-board');
+	for (var i=0; i<4; i++) {
+		var newDiv = document.createElement('div');
+		newDiv.className = "card";
+		gameBoard.appendChild(newDiv)
+	}
+};
+createCards();
