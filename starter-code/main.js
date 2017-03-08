0bdf51379704185e277f9220e5c87f170cdee339
Louis Boyle <louis_boyle@hotmail.com>
2017-03-08 16:32:33 +0000

    unit 11 assignment

--- starter-code/main.js
+++ starter-code/main.js
@@ -9,12 +9,56 @@ console.log("JS file is connected to HTML! Woo!")
 // } else {
 // 	alert("Sorry, try again.");
 // }
-var createCards = function() {
-	var gameBoard = document.getElementById('game-board');
-	for (var i=0; i<4; i++) {
+var cards = ['queen','queen','king','king'];
+var cardsInPlay = [];
+var gameBoard = document.getElementById('game-board');
+
+var createBoard = function() {
+	for (var i=0; i<cards.length; i++) {
 		var newDiv = document.createElement('div');
 		newDiv.className = "card";
-		gameBoard.appendChild(newDiv)
+        newDiv.setAttribute('data-card', cards[i]);
+		gameBoard.appendChild(newDiv);
+	}
+	for (var i=0; i<cards.length; i++) {
+		document.querySelectorAll('.card')[i].addEventListener('click', isTwoCards);
 	}
 };
-createCards();
+
+var isTwoCards = function() {
+	cardsInPlay.push(this.getAttribute('data-card'));
+	if (this.getAttribute('data-card') == 'queen') {
+		if ((Math.random()*100) > 50) {
+			this.innerHTML = '<img src="queen-card-black.png" alt="Queen"/>';
+		} else {
+			this.innerHTML = '<img src="queen-card-red.png" alt="Queen"/>';
+		}
+	} else {
+		if ((Math.random()*100) > 50) {
+			this.innerHTML = '<img src="king-card-black.png" alt="King"/>';
+		} else {
+			this.innerHTML = '<img src="king-card-red.png" alt="King"/>';
+		}
+	}
+	if (cardsInPlay.length == 2) {
+	    isMatch(cardsInPlay);
+	    cardsInPlay = [];
+	}
+};
+
+var isMatch = function(cardsInPlay) {
+	if(cardsInPlay[0] && cardsInPlay[1]) {
+		if ((cardsInPlay[0] === 'queen' && cardsInPlay[1] == 'queen') || (cardsInPlay[0] === 'king' && cardsInPlay[1] == 'king')) {
+			alert('match');
+		} else {
+			alert('no match');
+		}
+		for (var i=0; i<cards.length; i++) {
+			document.querySelectorAll('.card')[i].innerHTML = "";
+		}
+	}
+};
+
+createBoard();
+
+
